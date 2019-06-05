a3bb561ece017071e617550d714ffe62ee381c9d
Vladimir Bredikhin <brutal.v4@gmail.com>
2019-06-05 08:50:32 +0300

    initial commit

---
+++ src/main/java/at/home/MathUtils.java
@@ -0,0 +1,9 @@
+package at.home;
+
+public class MathUtils {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/test/java/at/home/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package at.home;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void test() {
+        System.out.println("This test ran");
+    }
+
+}
